b46358c1e4b6458f60cc1d97a7316a8116bab229
farhanaftab25 <farhanaftab25@gmail.com>
2026-03-18 14:21:14 +0500

    feat: implement player movement, spawning circles, and collision detection

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 import pygame
-from logger import log_state
+import sys
+
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
 from constants import *
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x,y)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     while(True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+        #print(dt)
+
 
 if __name__ == "__main__":
     main()
